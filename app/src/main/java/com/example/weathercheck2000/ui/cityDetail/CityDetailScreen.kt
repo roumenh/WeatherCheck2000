cff3ae1401097a7e6927218e410b60f314056066
Roman Hruška <hruska.r@gmail.com>
2024-03-10 21:49:56 +0100

    added preview

--- app/src/main/java/com/example/weathercheck2000/ui/cityDetail/CityDetailScreen.kt
+++ app/src/main/java/com/example/weathercheck2000/ui/cityDetail/CityDetailScreen.kt
@@ -1,8 +1,14 @@
 package com.example.weathercheck2000.ui.cityDetail;
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.weathercheck2000.R
+import com.example.weathercheck2000.WeatherCode
+import com.example.weathercheck2000.data.model.CurrentWeather
+import com.example.weathercheck2000.data.model.WeatherForecast
 
 @Composable
 fun CityDetailScreen(
@@ -23,4 +29,29 @@ fun CityDetailScreen(
     }
 
 
+}
+
+
+@Preview
+@Composable
+fun CityDetailScreenPreview(){
+    MaterialTheme {
+        CityDetailScreen(uiState =
+            CityDetailUiState(
+                forecast = WeatherForecast(
+                    todayMinTemperature = 5.0,
+                    todayMaxTemperature = 35.2
+                ),
+                current = CurrentWeather(
+                    temperature = 15.7,
+                    windSpeed = 178.0,
+                    windDirection = 65.7,
+                    weatherCode = WeatherCode(
+                        R.drawable.code0,
+                        "Clear sky"
+                    ),
+                )
+            )
+        )
+    }
 }
