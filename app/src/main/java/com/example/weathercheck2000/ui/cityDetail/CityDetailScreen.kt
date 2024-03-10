51ecc1779b47738acca5b85149bd902228230c8c
Roman Hruška <hruska.r@gmail.com>
2024-03-10 22:33:36 +0100

    City detail UI kinda ready

--- app/src/main/java/com/example/weathercheck2000/ui/cityDetail/CityDetailScreen.kt
+++ app/src/main/java/com/example/weathercheck2000/ui/cityDetail/CityDetailScreen.kt
@@ -1,10 +1,32 @@
 package com.example.weathercheck2000.ui.cityDetail;
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ShoppingCart
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.weathercheck2000.R
 import com.example.weathercheck2000.WeatherCode
 import com.example.weathercheck2000.data.model.CurrentWeather
@@ -14,18 +36,97 @@ import com.example.weathercheck2000.data.model.WeatherForecast
 fun CityDetailScreen(
     uiState: CityDetailUiState
 ) {
-    Column {
-        Text ("aha")
-        uiState.forecast?.let {
-            Text("today min: " + it.todayMinTemperature.toString() + " celsius")
-            Text("today max: " + it.todayMaxTemperature.toString() + " celsius")
+    Scaffold(
+        modifier = Modifier.padding(16.dp),
+        containerColor = Color.Transparent,
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(padding),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text (
+                text = "City name", //TODO replace with the actual city
+                style = MaterialTheme.typography.headlineLarge,
+            )
+
+            HorizontalDivider(Modifier.padding(vertical = 8.dp))
+
+            //Todays forecast
+            uiState.forecast?.let {
+
+                Text (
+                    modifier = Modifier.padding(top = 16.dp),
+                    text = stringResource(R.string.todays_forecast),
+                    style = MaterialTheme.typography.headlineSmall
+                )
+
+                    Row(
+                    modifier = Modifier.fillMaxWidth(),
+                ){
+                    Text(stringResource(R.string.minimum_temperature))
+                    Spacer(Modifier.weight(1f))
+                    Text(stringResource(R.string.celsius, it.todayMinTemperature.toString()))
+                }
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                ){
+                    Text(stringResource(R.string.maximum_temperature))
+                    Spacer(Modifier.weight(1f))
+                    Text(stringResource(R.string.celsius, it.todayMaxTemperature.toString()))
+                }
+
+            }
+
+            //Current weather
+            uiState.current?.let {
+
+                Text (
+                    modifier = Modifier.padding(top = 16.dp),
+                    text = stringResource(R.string.current_weather),
+                    style = MaterialTheme.typography.headlineSmall
+                )
+
+                Row(){
+                    Icon(
+                        painter = painterResource(R.drawable.thermostat_24px),
+                        contentDescription = stringResource(id = R.string.current_temperature)
+                    )
+                    Text(stringResource(R.string.celsius, it.temperature.toString()))
+                }
+
+
+                Image(
+                    modifier = Modifier
+                        .size(64.dp),
+                    painter = painterResource(id = it.weatherCode!!.imageId),
+                    contentDescription = it.weatherCode.description,
+                )
+
+                Spacer(Modifier.height(16.dp))
+
+                Row(){
+                    Icon(
+                        painter = painterResource(R.drawable.cyclone_24px),
+                        contentDescription = stringResource(id = R.string.current_temperature)
+                    )
+                    Text(stringResource(R.string.kilometers_per_hour, it.windSpeed.toString()))
+                }
+
+                Image(
+                    modifier = Modifier
+                        .size(64.dp)
+                        .rotate(it.windDirection.toFloat()),
+                    painter = painterResource(id = R.drawable.arrow),
+                    contentDescription = stringResource(R.string.kilometers_per_hour, it.windSpeed.toString()),
+                )
+
+            }
         }
-        uiState.current?.let {
-            Text("current: " + it.temperature.toString() + " celsius")
-            Text("wind: " + it.windDirection.toString() + " direction")
-            Text("wind: " + it.windSpeed.toString() + " speed")
-            Text("weather code: " + it.weatherCode.toString() + " ou ou")
-        }
+
     }
 
 
@@ -47,7 +148,7 @@ fun CityDetailScreenPreview(){
                     windSpeed = 178.0,
                     windDirection = 65.7,
                     weatherCode = WeatherCode(
-                        R.drawable.code0,
+                        R.drawable.code2,
                         "Clear sky"
                     ),
                 )
